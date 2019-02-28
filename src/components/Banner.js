df36e8af8d163a054e0867716843ebf981fb6bb2
Ramesh <rpaudel58@gmail.com>
2019-02-28 14:30:50 +0545

    Addded next prev behaviour to the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
 
-class Banner extends Component {    
+class Banner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: [
+                "http://lorempixel.com/1300/200/technology",
+                "http://lorempixel.com/1300/200/sports",
+                "http://lorempixel.com/1300/200/abstract",
+                "http://lorempixel.com/1300/200/nature"
+            ],
+            position: 0
+        }
+        
+    }
+
+    onNextClickHandler(){
+        if(this.state.position < this.state.images.length){
+            this.setState({
+                position: this.state.position + 1
+            });
+        }
+        console.log("Current image", this.state.images[this.state.position])
+    }
+    
+    onPrevClickHandler(){
+        console.log('[rev', this)
+        if(this.state.position > 0){
+            this.setState({
+                position: this.state.position - 1
+            });
+        }
+        console.log("Current poition", this.state.position)
+    }
+    
     render() {
         return (
             <div>
                 <img
-                    src={
-                        this.props.image ||
-                        "http://lorempixel.com/1300/200/technology"
-                    }
+                    src={this.state.images[this.state.position]}
                     alt=""
                 />
+                <button onClick={ () => this.onPrevClickHandler() }>Prev</button>
+                <button onClick={ this.onNextClickHandler.bind(this) }>Next</button>
                 
                 <section>{this.props.children};</section>
             </div> 
